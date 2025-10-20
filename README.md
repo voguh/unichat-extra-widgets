f032052d5301a6c0ea5fd6264a500c5bf8fdc3f1
Voguh <voguhofc@protonmail.com>
2025-10-20 16:08:56 -0300

    feat(WPGChat): implement new widget

--- README.md
+++ README.md
@@ -24,3 +24,30 @@ Um widget de chat inspirado no estilo do Windows 95.
     </tr>
   </tbody>
 </table>
+
+---
+
+### [WPGChat](./WPGChat)
+
+Um widget que funciona apenas no YouTube devido à utilização do `authorProfilePictureUrl`.
+Este widget de chat apresenta as mensagens como se fossem conversas em um grupo de whatsapp.
+
+<table>
+  <tbody>
+    <tr>
+      <td>
+        <img src="./docs/WPGChat_preview.gif" />
+      </td>
+      <td>
+        <h4>Pontos chave:</h4>
+        <ul>
+          <li>Avatar dos usuários;</li>
+          <li>Altera dinamicamente o lado em que o avatar dos usuários está;</li>
+          <li>Usuários comuns, sponsors e moderadores tem cores diferentes;</li>
+          <li>Os textos de mensagem de `donate`, `sponsor`, `sponsor_gift` e `raid` são customizáveis editando o `script.js`;</li>
+          <li>As cores podem ser alteradas editando o `style.css`;</li>
+        </ul>
+      </td>
+    </tr>
+  </tbody>
+</table>
---
+++ WPGChat/script.js
@@ -0,0 +1,164 @@
+const DONATE_TEMPLATE_MESSAGE = `Mandou <span>{currency} {value}</span>!`;
+const SPONSOR_TEMPLATE_MESSAGE = `Se tornou {if_platform(youtube,twitch)::membro::sub} por <span>{months}</span> meses!`;
+const SPONSOR_GIFT_TEMPLATE_MESSAGE = `Mandou <span>{count} {if_platform(youtube,twitch)::membros::subs}</span> de presente!`;
+const RAID_TEMPLATE_MESSAGE = `<span>{author_display_name}</span> acabou de chegar com {viewer_count} espectadores!`;
+const RAID_VIEWER_COUNT_DEFAULT = "seus";
+
+/* ================================================================================================================== */
+
+const MAIN_CONTAINER = document.querySelector("#main-container");
+const MESSAGE_TEMPLATE = document.querySelector("#chatlist_item").innerHTML;
+const DONATE_TEMPLATE = document.querySelector("#donate_item").innerHTML;
+const SPONSOR_TEMPLATE = document.querySelector("#sponsor_item").innerHTML;
+const SPONSOR_GIFT_TEMPLATE = document.querySelector("#sponsor-gift_item").innerHTML;
+const RAID_TEMPLATE = document.querySelector("#raid_item").innerHTML;
+
+function buildBadges(badges) {
+    let badgeJoin = ''
+
+    for (const badge of badges) {
+        badgeJoin+=`<img src="${badge.url}" class="badge" />`
+    }
+
+    return badgeJoin;
+}
+
+function buildMessage(message, emotes) {
+    if (message == null || typeof message !== "string" || message.trim().length === 0) {
+        return "";
+    }
+
+    let safeMessage = (message ?? "").replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+    if (!emotes || !Array.isArray(emotes) || emotes.length === 0) {
+        return safeMessage;
+    }
+
+    const emotesMap = new Map(emotes.map(emote => [emote.code, emote.url]));
+    const processedWords = safeMessage.split(' ').map(word => {
+        const emoteUrl = emotesMap.get(word);
+        return emoteUrl ? `<img src="${emoteUrl}" />` : word;
+    });
+
+    return processedWords.join(' ');
+}
+
+function parseTierName(platform, tier) {
+    if (platform === "twitch" && tier.toLowerCase() !== "prime") {
+        return parseInt(tier, 10) / 1000
+    }
+
+    return tier;
+}
+
+const platformConditionalRegExp = /\{if_platform\(([^)]+)\)::([^:}]*)::([^:{}]*)\}/g;
+function enrichMessage(text, data) {
+    let enrichedText = text;
+
+    for (const [rawKey, value] of Object.entries(data)) {
+        const key = `{${rawKey}}`;
+        const snakeKey = key.replace(/([A-Z])/g, "_$1").toLowerCase();
+
+        if (rawKey === "tier") {
+            enrichedText = enrichedText.replaceAll(key, parseTierName(data.platform, value));
+            enrichedText = enrichedText.replaceAll(snakeKey, parseTierName(data.platform, value));
+        } else if (rawKey === "platform") {
+            enrichedText = enrichedText.replaceAll(key, value);
+            enrichedText = enrichedText.replaceAll(snakeKey, value);
+            enrichedText = enrichedText.replace(platformConditionalRegExp, (match, platforms, option1, option2) => {
+                const [platform1, platform2] = platforms.split(',').map(p => p.trim());
+
+                if (value === platform1.trim()) {
+                    return option1;
+                } else if (value === platform2.trim()) {
+                    return option2;
+                }
+
+                return match;
+            });
+        } else if (rawKey === "viewerCount") {
+            enrichedText = enrichedText.replaceAll(key, value > 1 ? value : RAID_VIEWER_COUNT_DEFAULT);
+            enrichedText = enrichedText.replaceAll(snakeKey, value > 1 ? value : RAID_VIEWER_COUNT_DEFAULT);
+        } else if (rawKey === "messageText") {
+            enrichedText = enrichedText.replaceAll(key, buildMessage(value, data.emotes));
+            enrichedText = enrichedText.replaceAll(snakeKey, buildMessage(value, data.emotes));
+        } else if (rawKey === "authorBadges") {
+            enrichedText = enrichedText.replaceAll(key, buildBadges(value));
+            enrichedText = enrichedText.replaceAll(snakeKey, buildBadges(value));
+        } else {
+            enrichedText = enrichedText.replaceAll(key, value);
+            enrichedText = enrichedText.replaceAll(snakeKey, value);
+        }
+    }
+
+    return enrichedText;
+}
+
+function removeChildren() {
+    if(MAIN_CONTAINER.children.length > 50) {
+        MAIN_CONTAINER.firstChild.remove();
+        requestAnimationFrame(removeChildren);
+    }
+}
+
+// Dispatch every time when websocket is connected (or reconnected)
+window.addEventListener("unichat:connected", function () {
+    // This listener doesn't receive any data, actually it just notifies
+    // that connection is established or re-established.
+});
+
+window.addEventListener("unichat:event", function ({ detail: event }) {
+    if (event.type === 'unichat:remove_message') {
+        /** @type {import("../unichat").UniChatEventRemoveMessage['data']} */
+        const data = event.data;
+        MAIN_CONTAINER.querySelector(`div[data-id="${event.data.messageId}"]`)?.remove();
+    } else if (event.type === 'unichat:remove_author') {
+        /** @type {import("../unichat").UniChatEventRemoveAuthor['data']} */
+        const data = event.data;
+        const messages = MAIN_CONTAINER.querySelectorAll(`div[data-from="${data.authorId}"]`);
+        for (const message of (messages ?? [])) {
+            message.remove();
+        }
+    } else if (event.type === 'unichat:clear') {
+        MAIN_CONTAINER.innerHTML = '';
+    } else {
+        let htmlTemplate;
+
+        if (event.type === "unichat:message") {
+            /** @type {import("../unichat").UniChatEventMessage['data']} */
+            const data = event.data;
+
+            htmlTemplate = enrichMessage(MESSAGE_TEMPLATE, data);
+            htmlTemplate = htmlTemplate.replace("{side}", MAIN_CONTAINER.children.length % 2 ? "left" : "right");
+        } else if (event.type === "unichat:donate") {
+            /** @type {import("../unichat").UniChatEventDonate['data']} */
+            const data = event.data;
+
+            htmlTemplate = enrichMessage(DONATE_TEMPLATE, data);
+            htmlTemplate = htmlTemplate.replace("{donate_meta}", enrichMessage(DONATE_TEMPLATE_MESSAGE, data));
+        } else if (event.type === "unichat:sponsor") {
+            /** @type {import("../unichat").UniChatEventSponsor['data']} */
+            const data = event.data;
+
+            htmlTemplate = enrichMessage(SPONSOR_TEMPLATE, data);
+            htmlTemplate = htmlTemplate.replace("{sponsor_meta}", enrichMessage(SPONSOR_TEMPLATE_MESSAGE, data));
+        } else if (event.type === "unichat:sponsor_gift") {
+            /** @type {import("../unichat").UniChatEventSponsorGift['data']} */
+            const data = event.data;
+
+            htmlTemplate = enrichMessage(SPONSOR_GIFT_TEMPLATE, data);
+            htmlTemplate = htmlTemplate.replace("{sponsor_gift_meta}", enrichMessage(SPONSOR_GIFT_TEMPLATE_MESSAGE, data));
+        } else if (event.type === "unichat:raid") {
+            /** @type {import("../unichat").UniChatEventRaid['data']} */
+            const data = event.data;
+
+            htmlTemplate = enrichMessage(RAID_TEMPLATE, data);
+            htmlTemplate = htmlTemplate.replace("{raid_meta}", enrichMessage(RAID_TEMPLATE_MESSAGE, data));
+        }
+
+        if (htmlTemplate != null && MAIN_CONTAINER.querySelector(`div[data-id="${event.data.messageId}"]`) == null) {
+            $(MAIN_CONTAINER).append(htmlTemplate);
+        }
+    }
+
+    requestAnimationFrame(removeChildren);
+});
